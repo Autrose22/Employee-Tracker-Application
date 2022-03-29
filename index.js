526b9b03ad43d7e250c0f5f87a411a582d349e6f
autumnrose <Arosebarrios95@gmail.com>
2022-03-28 20:26:13 -0500

    stuff

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-const inquirer = require('inquirer');
-const mysql = require('mysql2');
-const db = require('./db/connection');
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+const consoleTable = require("console.table");
+
+const PORT = process.env.PORT || 3001;
+
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',      
+    password: 'password',
+    database: 'employee_db'
+  },
+  console.log('Connected to the employee_info database.')
+);
+
